e32aadafcf8ba304418e93f5c078b2cac2c5db69
Jenno <jiongtong06@gmail.com>
2026-01-27 17:56:57 -0500

    Feign代替restTemplate发送http请求： 1. 引入依赖 2. 添加@EnableFeignClients注解 3. 写FeignClient接口 4. 使用接口定义的方法

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -13,17 +14,32 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // 2.用RestTemplate发起用户请求
-        String url="http://userservice:/user/"+order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        // 2.用Feign发起用户请求
+        User user = userClient.findById(order.getUserId());
         // 3.封装user到Order
         order.setUser(user);
         // 4.返回
         return order;
     }
+
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public Order queryOrderById(Long orderId) {
+//        // 1.查询订单
+//        Order order = orderMapper.findById(orderId);
+//        // 2.用RestTemplate发起用户请求
+//        String url="http://userservice:/user/"+order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//        // 3.封装user到Order
+//        order.setUser(user);
+//        // 4.返回
+//        return order;
+//    }
 }
